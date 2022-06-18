2926cb47a8b71a3b0cf1edc29c1844239fa3082d
migvillabato <miguel.villanueva.lobato@gmail.com>
2022-06-18 13:36:45 +0200

    Test
    Test

--- core/state/dump.go
+++ core/state/dump.go
@@ -9,7 +9,7 @@
 // The go-ethereum library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
+// GNU Lesser General Public License for more details. villa
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
